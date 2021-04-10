395829abd254ac244b188a02a8f1f2a9c25d3d13
unknown <riccardo.cadei98@gmail.com>
2021-04-10 19:19:43 +0200

    add evaluate_model

--- training.py
+++ training.py
@@ -9,19 +9,19 @@ from other import *
 
 
 # returns a split in train and validation data
-def random_split(train_input, train_target, train_classes, percentage_valid=0.1):
+def random_split(train_input, train_target, train_classes, percentage_val=0.1):
     # shuffle data
     idx = torch.randperm(train_input.size(0))
     train_input = train_input[idx]
     train_target = train_target[idx]
     train_classes = train_classes[idx]
     # split 
-    train_size = math.floor(train_input.size(0) * (1 - percentage_valid))
-    valid_size = train_input.size(0) - train_size
-    train_input, valid_input = torch.split(train_input, [train_size, valid_size])
-    train_target, valid_target = torch.split(train_target, [train_size, valid_size])
-    train_classes, valid_classes = torch.split(train_classes, [train_size, valid_size])
-    return train_input, train_target, train_classes, valid_input, valid_target, valid_classes
+    train_size = math.floor(train_input.size(0) * (1 - percentage_val))
+    val_size = train_input.size(0) - train_size
+    train_input, val_input = torch.split(train_input, [train_size, val_size])
+    train_target, val_target = torch.split(train_target, [train_size, val_size])
+    train_classes, val_classes = torch.split(train_classes, [train_size, val_size])
+    return train_input, train_target, train_classes, val_input, val_target, val_classes
 
 # since our task is to predict whether the first channel of images in train_input
 # is lesser or equal than the second channel, we can flip the two channels and double our
@@ -45,60 +45,62 @@ def augment(train_input, train_target, train_classes):
 
 
 def run_experiment(model, nb_epochs = 25, weight_decay = 0.1, 
-                            mini_batch_size = 50, lr = 1e-3*0.5, percentage_valid=0.1, verbose=False):
+                            mini_batch_size = 50, lr = 1e-3*0.5, percentage_val=0.1, verbose=1):
 
     # device
     device = ('cuda' if torch.cuda.is_available() else 'cpu')
-    print("Device used: ", device)
+    if verbose>=1: print("Device used: ", device)
 
     # loading the data
     N = 1000 
     (train_input, train_target, train_classes,
      test_input, test_target, test_classes) = prologue.generate_pair_sets(N)
-    print("Loading training and test set...")
-
+    if verbose>=1: print("Loading training and test set...")
     # splitting the dataset and data augmentation
-    (train_input, train_target, train_classes,
-     valid_input, valid_target, valid_classes) = random_split(train_input, train_target, train_classes, percentage_valid)
-    print("Splitting the training set in training and validation set...")
+    (train_input, train_target, train_classes,val_input, val_target, val_classes) = random_split(train_input, train_target, train_classes, percentage_val)
+    if verbose>=1: print("Splitting the training set in training and validation set...")
     train_input, train_target, train_classes = augment(train_input, train_target, train_classes)
-    print("Data augmentation...")
-    print("In total there are: \n - {} samples in the Training Set ({} *2), \n - {} samples in the Validation Set, \n - {} samples in the Test Set"
-        .format(train_input.size(0), int((1-percentage_valid)*N), int(percentage_valid*N), N))
+    if verbose>=1: print("Data augmentation...")
+    if verbose>=1: print("In total there are: \n - {} samples in the Training Set ({} *2), \n - {} samples in the Validation Set, \n - {} samples in the Test Set"
+        .format(train_input.size(0), int((1-percentage_val)*N), int(percentage_val*N), N))
 
-    print('Number of parameters of the model: {}'.format(count_parameters(model)))
+    if verbose>=1: print('Number of parameters of the model: {}'.format(count_parameters(model)))
 
     # move to Device
     model.to(device)
     train_input.to(device)
     train_target.to(device)
-    valid_input.to(device)
-    valid_target.to(device)
+    val_input.to(device)
+    val_target.to(device)
     test_input.to(device)
     test_target.to(device)
     
     # training
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr = lr, weight_decay=weight_decay)
-    print('Training...')
-    train_losses, valid_losses = train(model, train_input, train_target, valid_input, valid_target, optimizer, criterion, 
+    if verbose>=1: print('Training...')
+    train_losses, val_losses = train(model, train_input, train_target, val_input, val_target, optimizer, criterion, 
                                         nb_epochs=nb_epochs,  mini_batch_size=mini_batch_size, verbose=verbose)
 
     # evaluate the performances
+    train_error = test(model, train_input, train_target)
+    if verbose>=1: print('\nTraining error: {0:.3f} %'.format(train_error*100) )
+    val_error = test(model, val_input, val_target)
+    if verbose>=1: print('Validation error: {0:.3f} %'.format(val_error*100) )
     test_error = test(model, test_input, test_target)
-    print('\nTest error: {0:.3f} %'.format(test_error*100) )
+    if verbose>=1: print('Test error: {0:.3f} %'.format(test_error*100) )
 
-    return train_losses, valid_losses, test_error
+    return train_losses, val_losses, (train_error, val_error, test_error)
 
 
 
-def train(model, train_input, train_target, valid_input, valid_target, 
-                 optimizer, criterion, nb_epochs = 25, mini_batch_size=50, verbose=True):
+def train(model, train_input, train_target, val_input, val_target, 
+                 optimizer, criterion, nb_epochs = 25, mini_batch_size=50, verbose=2):
     """
     Train model
     """
     train_losses = []
-    valid_losses = []
+    val_losses = []
     for epoch in range(nb_epochs):
         train_loss_e = 0
         num_batches = 0
@@ -121,15 +123,38 @@ def train(model, train_input, train_target, valid_input, valid_target,
         train_losses.append(train_loss)
 
         # validation
-        valid_preds = model(valid_input)
-        valid_loss = criterion(valid_preds, valid_target).data.item()
-        valid_losses.append(valid_loss)
+        val_preds = model(val_input)
+        val_loss = criterion(val_preds, val_target).data.item()
+        val_losses.append(val_loss)
 
-        if verbose:
-            print("Epoch", epoch+1, "/", nb_epochs, "train loss:", train_loss, "valid loss:", valid_loss)
+        if verbose==2:
+            print("Epoch", epoch+1, "/", nb_epochs, "train loss:", train_loss, "valid loss:", val_loss)
 
-    return train_losses, valid_losses
+    return train_losses, val_losses
 
+
+def evaluate_model(model, n, nb_epochs = 25, weight_decay = 0.1, 
+                    mini_batch_size = 50, lr = 1e-3*0.5, percentage_val=0.1, verbose=0):
+    train_errors = []
+    val_errors = []
+    test_errors = []
+    print('Number of experiments: {}'.format(n))
+    print('Computing...')
+    for i in range(n):
+        _, _, errors = run_experiment(model, nb_epochs = 25, verbose=verbose)
+        train_errors.append(errors[0])
+        val_errors.append(errors[1])
+        test_errors.append(errors[2])
+    mean_train_error = torch.mean(torch.Tensor(train_errors))
+    mean_val_error = torch.mean(torch.Tensor(val_errors))
+    mean_test_error = torch.mean(torch.Tensor(test_errors))
+    std_train_error = torch.std(torch.Tensor(train_errors))
+    std_val_error = torch.std(torch.Tensor(val_errors))
+    std_test_error = torch.std(torch.Tensor(test_errors))
+    print('Training Set: \n- Mean: {0:.3f},\n- Standard Error: {0:.3f}'.format(mean_train_error,std_train_error) )
+    print('Validation Set: \n- Mean: {0:.3f},\n- Standard Error: {0:.3f}'.format(mean_val_error,std_val_error) )
+    print('Test Set: \n- Mean: {0:.3f},\n- Standard Error: {0:.3f}'.format(mean_test_error,std_test_error) )
+    return
 
 def test(model, test_input, test_target):
     predict_classes_perc = model(test_input)
